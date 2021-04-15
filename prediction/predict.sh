c4b4dea07b40765a6fa59ca09432db5f5c640833
Changchang Liu <cliu03@g.harvard.edu>
2021-04-15 14:07:35 -0400

    predict from hdf with docked poses

---
+++ prediction/predict.sh
@@ -0,0 +1,1 @@
+python /data/cl321/git_repos/KI/prediction/hdf_poses//predict_apair.py --input /data/cl321/Datasets/D13/M2/I2/input_log/exx3/5/12-18-2020-16.txt --modelfile /data/cl321/Datasets/D13/M2/I2/modelfile.log --batch 128
